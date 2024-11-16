25f9fd04ee9445563fd01057469ebedc7ec4158d
Manu <164680971+WOOPERMANU@users.noreply.github.com>
2024-11-16 10:40:10 -0800

    Create Weaponstats.h

---
+++ Weaponstats.h
@@ -0,0 +1,19 @@
+#ifndef WEAPONSTATS.H
+#define WEAPONSTATS.H
+
+using namespace 
+
+class weaponstats 
+{
+   map<string, int> dict;
+
+    // Insert key-value pairs
+    dict["Beggers sword"] = 10;
+    dict["Travelers cleaver"] = 20;
+    dict["Knight's broadsword"] = 30;
+    dict["Tyrant's blade"] = 40;
+    dict["Exodia's Wrath"] = 50;
+    dict["Your mom's slipper"] = 1000000000000000000;
+};
+
+#endif
