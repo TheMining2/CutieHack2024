a6d79c3e9272c029b0962c369e002bcddf39c78f
TheMining2 <81710787+TheMining2@users.noreply.github.com>
2024-11-16 14:00:53 -0800

    Updated Character.h
    Added more functions

--- Character.h
+++ Character.h
@@ -1,14 +1,29 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+#include <unordered_map>
+#include <string>
+
 using namespace std;
 
 class Character{
   private:
-
+    string _name;
+    int _health;
+    int _strength;
+    unordered_map<string, int> _inventory;
   public:
-    Character();
-    int hp = 100;
+    Character(string name = "none", int health = 100, int strength = 1);
+    void attack(Character target) const;
+    void heal(int amount);
+    int getHealth() const;
+    int getStrength() const;
+    void setHealth(int amount);
+    void setStrength(int amount);
+    bool getItemInInventory(string& item);
+
+    friend ostream& operator<<(ostream& out, Character& in);
+    
 };
 
 #endif
