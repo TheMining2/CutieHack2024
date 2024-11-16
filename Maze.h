8fa67fe7cea4a9bfd42466cb8c13c7f70ceb754b
TheMining2 <81710787+TheMining2@users.noreply.github.com>
2024-11-16 10:26:51 -0800

    Updated Maze.h
    Added member variables & functions

--- Maze.h
+++ Maze.h
@@ -4,7 +4,16 @@
 using namespace std;
 
 class Maze{
-
+  public:
+    Maze();
+    void printMaze() const;
+    bool validMoveLocation() const;
+    
+  private:
+    vector<vector<int>> mazeMap;
+    vector<vector<int>> valueMap;
+    void generateMaze();
+    bool thereIsExit() const;
 };
 
 #endif
