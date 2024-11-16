3edef049ebf07d29b892995c279ec3d59c9eaa5a
TheMining2 <81710787+TheMining2@users.noreply.github.com>
2024-11-16 14:01:43 -0800

    Updated Character.cpp
    Defined all function definitions

--- Character.cpp
+++ Character.cpp
@@ -3,5 +3,45 @@
 
 using namespace std;
 
-Character::Character(){
+Character::Character(string name, int health, int strength) : _name(name), _health(health), _strength(strength){
+}
+
+void Character::attack(Character target) const{
+    target._health -= _strength;
+}
+
+void Character::heal(int amount){
+    _health += amount;
+}
+
+int Character::getHealth() const{
+    return _health;
+}
+
+int Character::getStrength() const{
+    return _strength;
+}
+
+void Character::setHealth(int amount){
+    _health = amount;
+}
+
+void Character::setStrength(int amount){
+    _strength = amount;
+}
+
+bool Character::getItemInInventory(string& item){
+    if (_inventory[item] > 0){
+        _inventory[item] -= 1;
+        return true;
+    } 
+    cout << item << " does not exist in your Inventory" << endl;
+    return false;
+}
+
+ostream& operator<<(ostream& out, Character& in){
+    out << '[' << in._name << "Stats]" << endl;
+    out << "Health: " << in._health << endl;
+    out << "Strength: " << in._strength << endl;
+    return out;
 }
